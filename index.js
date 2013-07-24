d87aebf10c88f2082194b8ed34df59a4bc9ec52f
Andri Möll <andri@dot.ee>
2013-07-24 17:57:31 +0300

    Allow overwriting locally and globally.

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 exports = module.exports = function() {}
 
 Object.defineProperty(Object.prototype, "must", {
-  set: function() {},
   get: function() { return new exports(this) },
+
+  set: function(value) {
+    Object.defineProperty(this, "must", {
+      value: value,
+      writable: true,
+      configurable: true,
+      enumrable: true
+    })
+  },
 
   // Without configurable, can't redefine it when reloading this file, e.g.
   configurable: true
--- test/index_test.js
+++ test/index_test.js
@@ -1,18 +1,51 @@
 // Bootstrapping with Assert ^_^:
 var assert = require("assert")
-var must = require("..")
+var Must = require("..")
+var Path = require("path")
 
-describe("Object", function() {
-  it("must have .must", function() {
-    assert(({}).must)
+describe("Object.prototype.must", function() {
+  it("must exist", function() {
+    assert(true.must)
   })
 
   it("must return an instance of Must", function() {
-    assert(({}).must instanceof must)
+    assert(true.must instanceof Must)
   })
 
-  it("must be have .must as reconfigurable", function() {
+  it("must be deletable", function() {
     delete Object.prototype.must
-    assert(!({}).must)
+    assert(!true.must)
+  })
+
+  it("must allow setting", function() {
+    var obj = {}
+    obj.must = 42
+    assert.equal(obj.must, 42)
+  })
+
+  afterEach(function() {
+    // Reload to reset the fiddling we did here.
+    delete require.cache[Path.resolve(__dirname, "../index.js")]
+    Must = require("..")
+  })
+})
+
+describe("Global.must", function() {
+  it("must be an instance of Must by default", function() {
+    assert(must instanceof Must)
+  })
+
+  it("must be settable to the must function", function() {
+    global.must = Must
+    assert(typeof must == "function")
+  })
+
+  it("must be settable to 42", function() {
+    global.must = 42
+    assert.equal(must, 42)
+  })
+
+  afterEach(function() {
+    delete global.must
   })
 })
