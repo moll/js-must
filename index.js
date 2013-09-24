19489c0f368e8b2e9ffb6da27a3d6b981ac30a73
Andri Möll <andri@dot.ee>
2013-09-24 22:36:44 +0300

    Remove unnecessary whitespace.

--- index.js
+++ index.js
@@ -99,5 +99,5 @@ function unbox(obj) {
   // references.
   return obj instanceof Boolean ||
          obj instanceof String ||
-         obj instanceof Number  ? obj.valueOf() : obj
+         obj instanceof Number ? obj.valueOf() : obj
 }
