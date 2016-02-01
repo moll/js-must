00a26f97b5fe7efaafc3bb74d8be8739859b9c3e
Radosław Nadstawny <radoslaw.nadstawny@gmail.com>
2016-02-01 22:49:23 +0100

    Expand tests for `permutationOf` to account for arrays containing objects

--- test/must/permutation_of_test.js
+++ test/must/permutation_of_test.js
@@ -2,24 +2,33 @@ var Must = require("../..")
 var assert = require("./assert")
 
 describe("Must.prototype.permutationOf", function() {
+  var o1 = {}
+  var o2 = {}
+  var o3 = {}
+
   it("must pass if given array has same members", function() {
     assert.pass(function() { Must([1, 2, 3]).be.a.permutationOf([3, 2, 1]) })
+    assert.pass(function() { Must([o1, o2, o3]).be.a.permutationOf([o3, o2, o1]) })
   })
 
   it("must fail if given array does not have same members", function() {
     assert.fail(function() { Must([1, 2, 3]).be.a.permutationOf([1]) })
+    assert.fail(function() { Must([o1, o2, o3]).be.a.permutationOf([o1]) })
   })
 
   it("must fail if given array is missing duplicated members", function() {
     assert.fail(function() { Must([1, 2]).be.a.permutationOf([2, 1, 1]) })
+    assert.fail(function() { Must([o1, o2]).be.a.permutationOf([o2, o1, o1]) })
   })
 
   it("must fail if given array has extra duplicated members", function() {
     assert.fail(function() { Must([1, 1, 2]).be.a.permutationOf([2, 1]) })
+    assert.fail(function() { Must([o1, o1, o2]).be.a.permutationOf([o2, o1]) })
   })
 
   it("must pass if given array has same duplicated members", function() {
     assert.pass(function() { Must([1, 1, 2]).be.a.permutationOf([2, 1, 1]) })
+    assert.pass(function() { Must([o1, o1, o2]).be.a.permutationOf([o2, o1, o1]) })
   })
 
   it("must pass if both arrays empty", function() {
